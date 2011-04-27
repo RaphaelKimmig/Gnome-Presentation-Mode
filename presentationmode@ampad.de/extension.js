2e9f2ef781adf1aad335d4c890db6f8644aa0ea8
Raphael Kimmig <raphael.kimmig@ampad.de>
2011-04-27 22:02:22 +0200

    initial commit

---
+++ presentationmode@ampad.de/extension.js
@@ -0,0 +1,50 @@
+const DBus = imports.dbus;
+const Main = imports.ui.main;
+const Lang = imports.lang;
+const PopupMenu = imports.ui.popupMenu;
+
+const Gettext = imports.gettext.domain('gnome-shell');
+const _ = Gettext.gettext;
+
+const SessionIface = {
+    name: "org.gnome.SessionManager",
+    methods: [ 
+    { name: "Inhibit", inSignature: "susu", outSignature: "u" },
+    { name: "Uninhibit", inSignature: "u", outSignature: "" }
+    ]
+};
+let SessionProxy = DBus.makeProxyClass(SessionIface);
+
+function main(extensionMeta) {
+    let Power = Main.Panel.STANDARD_TRAY_ICON_SHELL_IMPLEMENTATION['battery'];
+
+    Power.prototype.__init = Power.prototype._init;
+    Power.prototype._init = function () {
+        this.__init.apply(this, arguments);
+        this._inhibit = undefined;
+        this._sessionProxy = new SessionProxy(DBus.session, 'org.gnome.SessionManager', '/org/gnome/SessionManager');
+        this._presentationswitch = new PopupMenu.PopupSwitchMenuItem(_("Presentation mode"), false);
+        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+        this.menu.addMenuItem(this._presentationswitch);
+        this._presentationswitch.connect('toggled', Lang.bind(this, function() {
+            if(this._inhibit) {
+                this._sessionProxy.UninhibitRemote(this._inhibit);
+                this._inhibit = undefined;
+            } else {
+                try {
+                    this._sessionProxy.InhibitRemote("presentor",
+                        0, 
+                        "Presentation mode",
+                        9,
+                        Lang.bind(this, this._onInhibit));
+                } catch(e) {
+                    //
+                }
+            }
+
+        }));
+    };
+    Power.prototype._onInhibit = function(cookie) {
+        this._inhibit = cookie;
+    };
+};
